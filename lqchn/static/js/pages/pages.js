a3fbc6f6eff8359571cf35ba4efcd3191baf68ac
lqchn <cjm>
2014-07-27 15:02:30 +0800

    rweq
    rweq

--- lqchn/static/js/pages/pages.js
+++ lqchn/static/js/pages/pages.js
@@ -166,18 +166,13 @@ $(document).ready(function(){
 
 
     $("#com").click(function(){
-        alert("1");
         $("#modeul").children().remove();
-        alert("2");
         var file_name = "/static/file/comment/test2.html"
-        alert("3");
         $.ajax({
             url: file_name,
             dataType: 'html',
             success: function(data) {
-                alert("4");
                 $("#modeul").append(data);
-                alert("5");
             }
         });
     });
